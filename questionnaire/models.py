d0b9d2b1b9a2a01ad9afcac6fb7201b774167af8
pavel yakovlev <sir.iakowlew-pawel@yandex.ru>
2024-05-01 14:31:13 +0200

    and dump

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Questionnaire(BaseModel):
     name = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='questionnaires/', null=True)
+    img = models.ImageField(upload_to='questionnaires/', null=True, blank=True)
 
 
 class Block(BaseModel):
